b8dd92a51e02741850208ad8ca9d92b83b00c00a
vic42 <git@kcs.de>
2013-01-24 23:10:25 +0100

    get rid of unnecessary files again

--- LUFA-120730/LUFA/Drivers/USB/Class/Host/AndroidAccessoryClassHost.d
+++
@@ -1,5 +0,0 @@
-../../LUFA/Drivers/USB/Class/Host/AndroidAccessoryClassHost.o: \
- ../../LUFA/Drivers/USB/Class/Host/AndroidAccessoryClassHost.c \
- ../../LUFA/Drivers/USB/Class/Host/../../Core/USBMode.h
-
-../../LUFA/Drivers/USB/Class/Host/../../Core/USBMode.h:
